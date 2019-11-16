e9de0eb565b2aa517a23ac7bfa267c20851b69c6
Andy Smith <andyrsmith@gmail.com>
2019-11-16 23:29:53 +0000

    Hmm... better debugging.

--- src/main/kotlin/argoko/crack/identifiers.kt
+++ src/main/kotlin/argoko/crack/identifiers.kt
@@ -1,8 +1,5 @@
 package argoko.crack
 
-import kotlin.contracts.InvocationKind
-import kotlin.contracts.contract
-
 infix fun<T> List<T>.`±`(x : T ) = this + x
 
 infix fun<T,R> T.`⫸`( fn : (T) -> R) = fn(this)
@@ -23,16 +20,27 @@ inline fun <T, R> T.`⩥`(block: T.() -> R): R {
     return block()
 }
 
+fun<T> T.repeated(n : Int) = (0 until n).map { this }
+
+var visibility = false
+fun <T> T.debug(b : Boolean, block: (T) -> Unit) = if (b) { block(this); this } else this
+
 fun main() {
     val xs = ( listOf(1,2,3) `±` 10) + (listOf(4,5,6) `±` 20)
     println(xs)
     val x = 10 `⫸` { it + 20 } `⫸` { it * 2 }
     val x2 = 10 `≫` { this ard 10 } `≫` { this / 3 } `≫` { listify() }
+    val b = true
     val x3 = 10+10
+            .debug(b) { println("add $it")}
             .div(3)
-            .listify()
+            .debug(b) { println("dev $it")}
+            .repeated(10)
+            .debug(b) { println("repeat $it")}
             .filter { it > 20 }
+            .debug(b) { println("filter $it")}
             .sum()
+            .debug(b) { println("sum $it")}
     println("x is $x")
     println("x3 is $x3")
 }
